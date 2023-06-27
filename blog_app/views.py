6a59c02c715710851fa5349c561d08920eb2cf79
Dvrklex <dvrklex@gmail.com>
2023-06-27 03:11:00 -0300

    Homepage 60%

--- blog_app/views.py
+++ blog_app/views.py
@@ -5,7 +5,12 @@ def home(request):
     view_name= 'Home' 
     context = {
         "home_css":"blog_app/css/home.css",
+        "slider_css":"blog_app/css/slider.css",
+        "swiperBundleCss":"blog_app/css/swiper-bundle.min.css",
+        #Scripts 
+        "swiperBundleJs":"blog_app/js/swiper-bundle.min.js",
+        "sliderJs":"blog_app/js/slider.js",
         }
-    latest_posts = Post.objects.all().order_by('-created')[:3]
+    latest_posts = Post.objects.all().order_by('-created')[:5]
     print(latest_posts)
     return render(request, 'blog_app/home.html',{'view_name': view_name,"context":context,"latest_posts":latest_posts})
